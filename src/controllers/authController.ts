e84c89cd32e1d379f751cf85369e7d4377d3366f
Kent Blesario <kentb@codev.com>
2025-04-10 10:11:07 +0800

    [Login Endpoint (NH01)] -added register and login app logic, added userschema model, added user interface, utilized bcrypt to hash password, utlized jwt to tokenize authentication

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,11 +1,57 @@
 import { Request, Response } from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import User from "../models/authModel";
 
 export const register = async (req: Request, res: Response): Promise<void> => {
-	res.status(201).json({ msg: "Registration succesful" });
+  console.log("register", req.body);
+
+  // res.status(201).json({ msg: "Registration succesful" });
+  try {
+    const {
+      name,
+      email,
+      password,
+    }: { name: string; email: string; password: string } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      res.status(400).json({ message: "User already exists" });
+      return;
+    }
+
+    const user = new User({ name, email, password });
+    await user.save();
+
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (err: any) {
+    res.status(500).json({ message: err.message || "Server error" });
+  }
 };
 
 export const login = async (req: Request, res: Response): Promise<void> => {
-	res.json({ msg: "login Succesful" });
+  try {
+    const { email, password }: { email: string; password: string } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      res.status(400).json({ message: "Invalid credentials" });
+      return;
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      res.status(400).json({ message: "Invalid credentials" });
+      return;
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET as string, {
+      expiresIn: "1h",
+    });
+
+    res.json({ token });
+  } catch (err: unknown) {
+    const error = err instanceof Error ? err.message : "Unknown error";
+    res.status(500).json({ message: error });
+  }
 };
---
+++ src/models/authModel.ts
@@ -0,0 +1,23 @@
+import mongoose, { Schema } from 'mongoose';
+import { IUser } from '../interfaces/userInterface';
+import bcrypt from 'bcryptjs';
+
+const UserSchema: Schema<IUser> = new Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now }
+  });
+  
+  UserSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    try {
+      this.password = await bcrypt.hash(this.password, 10);
+      next();
+    } catch (err) {
+      next(err as Error);
+    }
+  });
+  
+  const User = mongoose.model<IUser>('User', UserSchema);
+  export default User;
